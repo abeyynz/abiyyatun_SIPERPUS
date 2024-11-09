10c17ebcdf1d26860873f9b68f893bf38335503e
abeyynz <abiyyatunnazihah@gmail.com>
2024-11-09 09:44:39 +0700

    pertemuan 5

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,15 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        
 
         $this->call([
-            BookShelfSeeder::class,
+            // BookShelfSeeder::class,
+            RoleSeeder::class,
+            PermissionSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
